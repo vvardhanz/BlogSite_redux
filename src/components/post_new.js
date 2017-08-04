403e2969572509426d9b37f715b93ce4745ba60a
vvardhan <kvvardhanz@gmail.com>
2017-08-03 23:10:13 -0400

    Added post/form

--- src/components/post_new.js
+++ src/components/post_new.js
@@ -1,12 +1,49 @@
 import React, { Component } from  'react';
+import { reduxForm } from 'redux-form';
+import { createPost } from '../actions/index';
 
 class PostsNew extends Component {
    render() {
-     return (
-    <div> this is my create new blog post page</div>
+        const { fields: { title, categories, content } , handleSubmit } = this.props;
 
+     return (
+        <form onSubmit={handleSubmit}>
+            <h3> Create a New Post </h3>
+            <div className="form-group">
+              <label>Title</lable>
+              <input type="text" className="form-control" {...title} />
+            </div>
+
+            <div className="form-group">
+               <label>Categories</label>
+               <input type="text" className="form-control" {...categories} />
+            </div>
+
+            <div className="form-control">
+               <label>Content</label>
+               <textarea className="form-control" {...content} />
+           </div>
+
+            <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
      );
    }
 }
 
-export default PostsNew;
+function validate(values) {
+   const errors = {};
+
+   if(!values.title) {
+       errors.title = 'Enter a username';
+   }
+
+   return errors;
+}
+
+
+//connect: first argument is mapStateToProps,  2nd is mapDispatchToProps
+//reduxForm: 1st is form config, 2nd is mapStateToProps, 3rd is mapDispatchToProps
+export default reduxForm({
+  form: 'PostNewForm',
+  fields: ['title', 'categories', 'content']
+}, null, { createPost })(PostsNew);
